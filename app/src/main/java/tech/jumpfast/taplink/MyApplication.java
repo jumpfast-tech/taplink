814a37208e54ee61e0ce0293544722a63c89087d
Chirag Jain <chirag@jumpfast.tech>
2024-12-15 01:12:35 +0530

    init base activity with fragments

---
+++ app/src/main/java/tech/jumpfast/taplink/MyApplication.java
@@ -0,0 +1,12 @@
+package tech.jumpfast.taplink;
+
+import android.app.Application;
+
+public class MyApplication {
+
+    private static Application sApplication;
+
+    public static Application getApplication() {
+        return sApplication;
+    }
+}
